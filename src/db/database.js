00ccc29480de473051df8d6b06e84f610462948e
Daniel Andersson <d.andersson86@icloud.com>
2021-09-13 18:50:40 +0200

    Setting up database connection with MongoDB cluster.

--- src/db/database.js
+++ src/db/database.js
@@ -1,10 +1,11 @@
 const mongo = require("mongodb").MongoClient;
 const config = require("./setup.json");
 const collectionName = "docs";
+const config = require("./config.json");
 
 const database = {
     getDb: async function getDb () {
-        let dsn = `mongodb://localhost:27017/mumin`;
+        let dsn = `mongodb+srv://texteditor:${config.username}@${config.password}.c1ix7.mongodb.net/mumin?retryWrites=true&w=majority`;
 
         if (process.env.NODE_ENV === 'test') {
             dsn = "mongodb://localhost:27017/test";

90eecf5a705ff6521486c1d69d0c568132c42ca6
Daniel Andersson <d.andersson86@icloud.com>
2021-09-18 00:23:40 +0200

    Created mongoose models for documents from texteditor.

---
+++ src/db/models/document.js
@@ -0,0 +1,45 @@
+/**
+ * Import Module Dependencies.
+ */
+const mongoose = require("mongoose");
+
+
+/**
+ * Schema for text documents.
+ */
+const document = new mongoose.Schema({
+    author: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    category: {
+        type: String,
+        required: false
+    },
+    text: {
+        type: String,
+        required: false
+    },
+    status: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true });
+
+
+/**
+ * Create Model from defined Schema.
+ */
+const Document = mongoose.model('document', document);
+
+
+/**
+ * Module Exports.
+ * @type {module:mongoose.Schema<Document, Model<Document, any, any>, undefined, {}>}
+ */
+module.exports = { Document, document };
